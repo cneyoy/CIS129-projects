f03229ad2073626ba9cd0a73e616c3b48d8c4fb7
cneyoy <159074203+cneyoy@users.noreply.github.com>
2024-02-07 17:21:44 -0700

    Update cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,2 +1,5 @@
+#author carlos neyoy
+# My first Python lab in my CIS129 course
+# Prints a simple short story
 print('there once was a dog named pyfa. the dog went on a walk one day trying to find food or someone to play with. instead the dog found that life was hard and food and play come rarely to a lonsesome dog. one day he picked up the scent of meat raw and red and so darted off following his nose. when the scent was near he found it to be an old man sizzling meats on a grill. At first the dog was weary of the stranger but when there eyes met they became fast friends and the dog found both food and play.')
 
